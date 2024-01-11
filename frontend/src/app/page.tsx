f643bca5742d392934a7e000e96b594ce6459f6c
ravindu0823 <guestpc87@gmail.com>
2024-01-11 22:33:45 +0530

    Refactor page component and add registration form

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,28 +1,28 @@
-import Image from 'next/image'
-import ProgressTracker from './components/ProgressTracker'
+import Image from "next/image";
+import ProgressTracker from "./components/ProgressTracker";
+import RegistrationForm from "./components/LoginForm";
 
 export default function Home() {
   return (
     <>
       <div className="w-full flex">
         <div className="hidden lg:flex w-[40%] bg-[#f27c37] h-screen items-center justify-center">
-          <Image src="/resp_logo.png" alt="Picture of the author" width={150} height={150} />
+          <Image
+            src="/resp_logo.png"
+            alt="Picture of the author"
+            width={150}
+            height={150}
+          />
         </div>
-        <div className='w-full lg:w-[60%]'>
-          <div className='ps-36 pt-20'>
-            <p className='text-3xl font-bold text-[#f27c37]'>Login Details</p>
-            <p className='text-gray-500'>Resper Sys.</p>
+        <div className="w-full lg:w-[60%]">
+          <div className="ps-36 pt-20">
+            <p className="text-3xl font-bold text-[#f27c37]">Login Details</p>
+            <p className="text-gray-500">Resper Sys.</p>
           </div>
-          <ProgressTracker/>
-          
-          <div>
-
-
-          </div>
+          <ProgressTracker />
+          <RegistrationForm />
         </div>
-
       </div>
-
     </>
-  )
+  );
 }
